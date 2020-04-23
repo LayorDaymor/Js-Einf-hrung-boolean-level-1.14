20dc91e34e947a199e8810e4877d696d947e4941
SuperCode <supercode@SuperCodes-MacBook-Air.local>
2020-04-24 00:12:50 +0200

    first updates

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+/* let x= 100 ;
+let x= 3.14 ;
+let x= -15;
+let x= "hello";
+let x= "false";
+let x= 1 + 7 + 3.14
+let x= 0 */
+let x= -0
+/* let x= NaN 
+let x= null
+let x=false
+let x=NaN */
+
+
+
+
